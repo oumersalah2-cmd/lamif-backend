5ba015d9f98e3101fe909adf334c08961217c7ff
Abdusalam Oumer <oumersalah2@gmail.com>
2026-04-11 11:30:19 +0300

    connect express server to mongodb database

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log('MongoDB error:', err))
+
 app.get(`/`, (req, res ) => {
     res.send('LAMIF API is running')
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-    console.log('Server is running on port ${PORT}')
+    console.log(`Server running on port ${PORT}`)
 })
