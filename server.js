4f0071f9dd8029ae81551d11cb26efc41456db07
Abdusalam Oumer <oumersalah2@gmail.com>
2026-04-11 15:15:29 +0300

    add user model and register route

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+const authRoutes = require('./routes/auth')
+
 const app = express()
+
+
 
 app.use(cors())
 app.use(express.json())
 
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.log('MongoDB error:', err))
+
+app.use('/api/auth', authRoutes)
 
 app.get(`/`, (req, res ) => {
     res.send('LAMIF API is running')
