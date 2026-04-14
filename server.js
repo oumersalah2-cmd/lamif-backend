5151969388783386bd9579f78d8931ba43e02b58
Abdusalam Oumer <oumersalah2@gmail.com>
2026-04-14 11:24:21 +0300

    add auth middleware and protected routes

--- server.js
+++ server.js
@@ -28,3 +28,9 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const protected = require(`./middleware/authMiddleware`)
+
+app.get(`/api/protected`, protected, (req, res) => {
+    res.json({ message: 'you are authorized!', user: req.user})
+})
