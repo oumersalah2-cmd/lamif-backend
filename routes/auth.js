6442af254eb2319b717588670347c0513145a113
Abdusalam Oumer <oumersalah2@gmail.com>
2026-04-14 11:02:08 +0300

    add login route with JWT token

--- routes/auth.js
+++ routes/auth.js
@@ -31,6 +31,41 @@ router.post('/register', async (req, res) => {
     }
 })
 
+router.post(`/login`, async (req, res) => {
+    try{
+        const { email,password } = req.body
+
+        const user = await User.findOne({ email })
+        if (!user ) {
+            return res.status(400).json({ message: 'Invalid credentials' })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' })
+        }
+
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: '7d' }
+        )
+
+        res.status(200).json({
+            message: 'Login successful',
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        })
+    
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' })
+    }
+        })
 module.exports = router
 
 
